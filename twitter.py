abfabf285ec6b33074d5e1e0b285d18113ac0e97
Quietscher <76123025+Quietscher@users.noreply.github.com>
2022-07-15 19:19:00 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -181,10 +181,10 @@ consumer_secret = ''
 access_token = ''
 access_token_secret = ''
 
-#auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-#auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-#api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)# https://developer.twitter.com/en/docs/twitter-api/rate-limits -> 1 tweet per sec
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)# https://developer.twitter.com/en/docs/twitter-api/rate-limits -> 1 tweet per sec
 
 
 numOfTotalTweets = 10000
