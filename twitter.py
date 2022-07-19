b070a63c324459b282df8297b3e15a0deeea62a1
Quietscher <76123025+Quietscher@users.noreply.github.com>
2022-07-19 18:14:14 +0200

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -7,224 +7,6 @@ import pandas as pd
 import tweepy
 from configparser import ConfigParser
 
-# Demo Info to test script
-sample = {
-    "data": [
-        {
-            "id": "1212092628029698048",
-            "text": "We believe the best future version of our API will come from 'building' it with YOU. Here’s to another great year with everyone, who builds on the Twitter platform. We can’t wait to continue working with you in the new year. https://t.co/yvxdK6aOo2",
-            "possibly_sensitive": "false",
-            "referenced_tweets": [
-                {
-                    "type": "replied_to",
-                    "id": "1212092627178287104"
-                }
-            ],
-            "entities": {
-                "urls": [
-                    {
-                        "start": 222,
-                        "end": 245,
-                        "url": "https://t.co/yvxdK6aOo2",
-                        "expanded_url": "https://twitter.com/LovesNandos/status/1211797914437259264/photo/1",
-                        "display_url": "pic.twitter.com/yvxdK6aOo2"
-                    }
-                ],
-                "annotations": [
-                    {
-                        "start": 144,
-                        "end": 150,
-                        "probability": 0.626,
-                        "type": "Product",
-                        "normalized_text": "Twitter"
-                    }
-                ]
-            },
-            "author_id": "2244994945",
-            "public_metrics": {
-                "retweet_count": 8,
-                "reply_count": 2,
-                "like_count": 40,
-                "quote_count": 1
-            },
-            "lang": "en",
-            "created_at": "2019-12-31T19:26:16.000Z",
-            "source": "Twitter Web App",
-            "in_reply_to_user_id": "2244994945",
-            "attachments": {
-                "media_keys": [
-                    "16_1211797899316740096"
-                ]
-            },
-            "context_annotations": [
-                {
-                    "domain": {
-                        "id": "119",
-                        "name": "Holiday",
-                        "description": "Holidays like Christmas or Halloween"
-                    },
-                    "entity": {
-                        "id": "1186637514896920576",
-                        "name": " New Years Eve"
-                    }
-                },
-                {
-                    "domain": {
-                        "id": "119",
-                        "name": "Holiday",
-                        "description": "Holidays like Christmas or Halloween"
-                    },
-                    "entity": {
-                        "id": "1206982436287963136",
-                        "name": "Happy New Year: It’s finally 2020 everywhere!",
-                        "description": "Catch fireworks and other celebrations as people across the globe enter the new year.\nPhoto via @GettyImages "
-                    }
-                },
-                {
-                    "domain": {
-                        "id": "46",
-                        "name": "Brand Category",
-                        "description": "Categories within Brand Verticals that narrow down the scope of Brands"
-                    },
-                    "entity": {
-                        "id": "781974596752842752",
-                        "name": "Services"
-                    }
-                },
-                {
-                    "domain": {
-                        "id": "47",
-                        "name": "Brand",
-                        "description": "Brands and Companies"
-                    },
-                    "entity": {
-                        "id": "10045225402",
-                        "name": "Twitter"
-                    }
-                },
-                {
-                    "domain": {
-                        "id": "119",
-                        "name": "Holiday",
-                        "description": "Holidays like Christmas or Halloween"
-                    },
-                    "entity": {
-                        "id": "1206982436287963136",
-                        "name": "Happy New Year: It’s finally 2020 everywhere!",
-                        "description": "Catch fireworks and other celebrations as people across the globe enter the new year.\nPhoto via @GettyImages "
-                    }
-                }
-            ]
-        }
-    ],
-    "includes": {
-        "tweets": [
-            {
-                "possibly_sensitive": "false",
-                "referenced_tweets": [
-                    {
-                        "type": "replied_to",
-                        "id": "1212092626247110657"
-                    }
-                ],
-                "text": "These launches would not be possible without the feedback you provided along the way, so THANK YOU to everyone who has contributed your time and ideas. Have more feedback? Let us know ⬇️ https://t.co/Vxp4UKnuJ9",
-                "entities": {
-                    "urls": [
-                        {
-                            "start": 187,
-                            "end": 210,
-                            "url": "https://t.co/Vxp4UKnuJ9",
-                            "expanded_url": "https://twitterdevfeedback.uservoice.com/forums/921790-twitter-developer-labs",
-                            "display_url": "twitterdevfeedback.uservoice.com/forums/921790-…",
-                            "images": [
-                                {
-                                    "url": "https://pbs.twimg.com/news_img/1261301555787108354/9yR4UVsa?format=png&name=orig",
-                                    "width": 100,
-                                    "height": 100
-                                },
-                                {
-                                    "url": "https://pbs.twimg.com/news_img/1261301555787108354/9yR4UVsa?format=png&name=150x150",
-                                    "width": 100,
-                                    "height": 100
-                                }
-                            ],
-                            "status": 200,
-                            "title": "Twitter Developer Feedback",
-                            "description": "Share your feedback for the Twitter developer platform",
-                            "unwound_url": "https://twitterdevfeedback.uservoice.com/forums/921790-twitter-developer-labs"
-                        }
-                    ]
-                },
-                "author_id": "2244994945",
-                "public_metrics": {
-                    "retweet_count": 3,
-                    "reply_count": 1,
-                    "like_count": 17,
-                    "quote_count": 0
-                },
-                "lang": "en",
-                "created_at": "2019-12-31T19:26:16.000Z",
-                "source": "Twitter Web App",
-                "in_reply_to_user_id": "2244994945",
-                "id": "1212092627178287104"
-            }
-        ]
-    }
-}
-
-demo_user = {
-    "data": [
-        {
-            "id": "2244994945",
-            "name": "Twitter Dev",
-            "username": "TwitterDev",
-            "location": "127.0.0.1",
-            "entities": {
-                "url": {
-                    "urls": [
-                        {
-                            "start": 0,
-                            "end": 23,
-                            "url": "https://t.co/3ZX3TNiZCY",
-                            "expanded_url": "/content/developer-twitter/en/community",
-                            "display_url": "developer.twitter.com/en/community"
-                        }
-                    ]
-                },
-                "description": {
-                    "hashtags": [
-                        {
-                            "start": 23,
-                            "end": 30,
-                            "tag": "DevRel"
-                        },
-                        {
-                            "start": 113,
-                            "end": 130,
-                            "tag": "BlackLivesMatter"
-                        }
-                    ]
-                }
-            },
-            "verified": "true",
-            "description": "The voice of Twitter's #DevRel team, and your official source for updates, news, & events about Twitter's API. \n\n#BlackLivesMatter",
-            "url": "https://t.co/3ZX3TNiZCY",
-            "profile_image_url": "https://pbs.twimg.com/profile_images/1267175364003901441/tBZNFAgA_normal.jpg",
-            "protected": "false",
-            "pinned_tweet_id": "1255542774432063488",
-            "created_at": "2013-12-14T04:35:55.000Z"
-        }
-    ],
-    "includes": {
-        "tweets": [
-            {
-                "id": "1255542774432063488",
-                "text": "During these unprecedented times, what’s happening on Twitter can help the world better understand &amp; respond to the pandemic. \n\nWe're launching a free COVID-19 stream endpoint so qualified devs &amp; researchers can study the public conversation in real-time. https://t.co/BPqMcQzhId"
-            }
-        ]
-    }
-}
-
 # Load twitter developer keys and tokens through a local config file
 config = ConfigParser()
 config.read('./keys.cfg')
@@ -242,7 +24,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True) #, wait_on_rate_limit_notify=True
 
 # Set total number of retrieved tweets
-numOfTotalTweets = 1000
+numOfTotalTweets = 100
 
 # Twitter Dataset must be in the same directory as the script
 dirs = next(os.walk("RussoUkrainianWar_Dataset-main"))[1] 
@@ -310,7 +92,7 @@ for date in tweet_ids:
     for tweet_id in tweet_ids[date]:
         tweet_obj = {}
         counter = counter + 1
-        print("Trying to fetch data for tweet No."+counter)
+        print("Trying to fetch data for tweet No."+str(counter))
         try:
             tweet = api.get_status(tweet_id)
         except tweepy.errors.NotFound:
@@ -324,9 +106,7 @@ for date in tweet_ids:
             print("other error occured")
             print("skipping id:"+ tweet_id)
             continue
-        tweet_obj["tweet"] = tweet
-        #tweet = sample
-        user = demo_user
+        tweet_obj["tweet"] = tweet    
         user = api.get_user(user_id = tweet.user.id) 
         tweet_obj["user"] = user
         arr.append(tweet_obj)
